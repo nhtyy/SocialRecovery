4ba5e6a9ee9620baa9228bb4063af8ddfb1a9554
nutty <mrnatbus12@gmail.com>
2022-05-29 20:58:20 -0700

    names

--- src/Vault.sol
+++ src/Vault.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-contract Vault {
+contract VaultWallet {
 
 ///=============================================================================================
 /// Struct
